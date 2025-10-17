dbc2a08fb5be4b093141a7e2b4b783cad9290183
al3xandr3-bruno <brunoalexdias15@gmail.com>
2025-10-17 08:04:33 -0300

    17/10/25

---
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.di.notificacao;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("dev")
+@TipoDoNotificador(NivelUrgencia.SEM_PRIORIDADE)
+@Component
+public class NotificadorEmailMock implements Notificador {
+
+	public NotificadorEmailMock(){
+		System.out.println("NotificadorEmail MOCK"); //apenas pra conferir que é ele mesmo que é instanciado
+	}
+
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n",
+				cliente.getNome(), cliente.getEmail(), mensagem);
+	}
+
+}
--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -2,19 +2,33 @@ package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 
-@Component
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+//@Component
 public class AtivacaoClienteService {
 
 	@TipoDoNotificador(NivelUrgencia.SEM_PRIORIDADE)
 	@Autowired
 	private Notificador notificador;
 
+//	@PostConstruct
+	public void init(){
+		System.out.println("INIT" + notificador);
+	}
+
+	//@PreDestroy
+	public void destroy(){
+		System.out.println("DESTROY");
+	}
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		notificador.notificar(cliente, "O cadastro no sistema está ativo");
